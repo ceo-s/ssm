0734b2395b65e5057a0e9feb65bffd798fdc0206
ceo-s <alex_s_2002@mail.ru>
2024-06-10 07:37:36 +0800

    Add case insensitve grepping

--- list_users.sh
+++ list_users.sh
@@ -2,7 +2,7 @@ source ./functions.sh
 
 SERVER_NAME=$1
 SERVER_INFO=$(get_server_configuration $SERVER_NAME)
-USERS_LIST_STRING=$(echo "$SERVER_INFO" | grep USERS | cut -d '=' -f 2 | sed 's/^ *//g' | tr ' ' '\n')
+USERS_LIST_STRING=$(echo "$SERVER_INFO" | grep -i users | cut -d '=' -f 2 | sed 's/^ *//g' | tr ' ' '\n')
 echo
 echo "List of valid users for $SERVER_NAME:"
 echo
--- login.sh
+++ login.sh
@@ -5,7 +5,7 @@ USER_NAME=$1
 SERVER_NAME=$2
 SERVER_INFO=$(get_server_configuration $SERVER_NAME)
 
-SERVER_IP=$(echo "$SERVER_INFO" | grep IP | cut -d '=' -f 2 | tr -d ' ')
+SERVER_IP=$(echo "$SERVER_INFO" | grep -i ip | cut -d '=' -f 2 | tr -d ' ')
 
 
 ssh $USER_NAME@$SERVER_IP
